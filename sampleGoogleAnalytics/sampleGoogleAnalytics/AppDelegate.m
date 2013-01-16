b9004d0ce39e647d6b77fdbbb93d8f7981d5b1d7
Pollapee Sroysuwan <im.oab.ps@gmail.com>
2013-01-16 10:05:17 +0700

    add hello world

--- sampleGoogleAnalytics/sampleGoogleAnalytics/AppDelegate.m
+++ sampleGoogleAnalytics/sampleGoogleAnalytics/AppDelegate.m
@@ -52,4 +52,9 @@
 	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (void) sayHelloWorld
+{
+    NSLog(@"Hello,World");
+}
+
 @end
