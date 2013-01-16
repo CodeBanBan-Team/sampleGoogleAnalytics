bc0af0133624bb491fd2b05cd9743a95d8ec2bd1
Pollapee Sroysuwan <im.oab.ps@gmail.com>
2013-01-16 11:21:34 +0700

    create test target

---
+++ sampleGoogleAnalytics/test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Pollapee Sroysuwan on 1/16/56 BE.
+//  Copyright (c) 2556 Code BanBan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ sampleGoogleAnalytics/test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test
+//
+//  Created by Pollapee Sroysuwan on 1/16/56 BE.
+//  Copyright (c) 2556 Code BanBan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
