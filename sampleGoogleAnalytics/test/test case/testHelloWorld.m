acb31dc0f9b0625a8850a7882dfe9490df717dd1
Pollapee Sroysuwan <im.oab.ps@gmail.com>
2013-01-18 14:40:06 +0700

    add test case for mockito

--- sampleGoogleAnalytics/test/test case/testHelloWorld.m
+++ sampleGoogleAnalytics/test/test case/testHelloWorld.m
@@ -9,6 +9,12 @@
 #import <GHUnitIOS/GHUnitIOS.h>
 #import "SayHelloWorld.h"
 
+#define HC_SHORTHAND
+#import <OCHamcrestIOS/OCHamcrestIOS.h>
+
+#define MOCKITO_SHORTHAND
+#import <OCMockitoIOS/OCMockitoIOS.h>
+
 @interface testHelloWorld : GHTestCase
 {
     SayHelloWorld* mHello;
@@ -45,5 +51,23 @@
     sayResult = [mHello sayHelloWorld:NO];
     GHAssertFalse(sayResult, @"sayHelloWorld return YES, It should be NO.");
     
+}
+
+- (void)testMockito
+{
+    
+    // mock creation
+    NSArray *mockArray = mock([NSArray class]);
+    
+    // stubbing
+    [given([mockArray objectAtIndex:0]) willReturn:@"first"];
+    
+    // following prints "(null)" because objectAtIndex:999 was not stubbed
+    GHAssertEqualStrings([mockArray objectAtIndex:0], @"first", @"[mockArray objectAtIndex:0] didn't return first");
+    
+    [given(mockArray[0]) willReturn:@"first"];
+    
+    // following prints "(null)" because objectAtIndex:999 was not stubbed
+    GHAssertEqualStrings(mockArray[0], @"first", @"mockArray[0] didn't return first");
 }
 @end
